288f412f5545f25c6b4bc787895c93387156106c
Fabrício Vasconcellos <fbrcsgo@gmail.com>
2021-03-22 20:08:09 -0300

    Fix recipe model

--- src/model/recipeModel.py
+++ src/model/recipeModel.py
@@ -6,6 +6,6 @@ db = SQLAlchemy()
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
-    steps = db.Column(db.Integer, nullable=False)
+    ingredients = db.Column(db.String(1000), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
