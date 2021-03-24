4b48d1d140f11440c6c7a2f6e88dd20750fa62f7
Fabrício Vasconcellos <fbrcsgo@gmail.com>
2021-03-23 22:44:11 -0300

    Implement create and update

--- src/controller/ingredientController.py
+++ src/controller/ingredientController.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 ingredient_controller = Blueprint('ingredient', __name__, url_prefix='/ingredients')
 
@@ -9,7 +9,23 @@ def index():
     return render_template('ingredient/ingredient.html', title='Ingredientes', data={'ingredients': ingredients})
 
 
-@ingredient_controller.route('/getIngredients', methods=['GET'])
+@ingredient_controller.route('/create', methods=['GET', 'POST'])
+def create_ingredients():
+    if request.method == 'POST':
+        ingredients = request.form.get('ingredients')
+    return render_template('ingredient/create-ingredient/create-ingredient.html', title='Adicionar ingredientes')
+
+
+@ingredient_controller.route('/edit', methods=['GET', 'PUT'])
+def update_ingredients():
+    stored_ingredients = get_ingredients()
+    if request.method == 'PUT':
+        ingredients = request.form.get('ingredients')
+    return render_template('ingredient/edit-ingredient/edit-ingredient.html', title='Editar ingredientes',
+                           data={'ingredients': stored_ingredients})
+
+
+@ingredient_controller.route('/getIngredients')
 def get_ingredients():
     return [
         {
--- src/controller/recipeController.py
+++ src/controller/recipeController.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
+from src.controller.ingredientController import get_ingredients
 
 recipe_controller = Blueprint('recipe', __name__, url_prefix='/recipes')
 
@@ -7,6 +9,23 @@ recipe_controller = Blueprint('recipe', __name__, url_prefix='/recipes')
 def index():
     recipes = get_recipes()
     return render_template('recipe/recipe.html', title='Receitas', data={'recipes': recipes})
+
+
+@recipe_controller.route('/create', methods=['GET', 'POST'])
+def create_recipe():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        ingredients = request.form.get('ingredients')
+
+    stored_ingredients = get_ingredients()
+    return render_template('recipe/create-recipe/create-recipe.html', data={'stored_ingredients': stored_ingredients})
+
+
+@recipe_controller.route('/edit/<id>')
+def get_recipe_by_id(id):
+    recipes = get_recipes()
+    recipe = find_recipe(recipes, id)
+    return render_template('recipe/edit-recipe/edit-recipe.html', data={'recipe': recipe})
 
 
 @recipe_controller.route('/getRecipes', methods=['GET'])
@@ -20,6 +39,7 @@ def get_recipes():
                 'Oléo': '1 xícara',
                 'Fubá': '1 xícara'
             },
+            'steps': '1 - Misturar, 2 - Bater, 3 - Degustar',
             'created-at': '22/03/2021'
         },
         {
@@ -31,6 +51,14 @@ def get_recipes():
                 'Fubá': '1 xícara',
                 'Leite condensado': '1'
             },
+            'steps': '1 - Misturar, 2 - Bater, 3 - Degustar',
             'created-at': '22/03/2021'
         }
     ]
+
+
+def find_recipe(recipe_list, recipe_id):
+    recipe_id = int(recipe_id)
+    for recipe in recipe_list:
+        if recipe['id'] == recipe_id:
+            return recipe
